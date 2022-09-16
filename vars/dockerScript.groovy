17a8795c7e33d1629912c17b34151a42c13cf2cc
artisantek <113725746+artisantek@users.noreply.github.com>
2022-09-17 02:05:23 +0530

    Add files via upload

---
+++ vars/dockerScript.groovy
@@ -0,0 +1,57 @@
+def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
+
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+		registry = "$registryin" 	
+		dockerTag = "${docTag}$BUILD_NUMBER"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+	}
+		
+	agent none
+	
+	stages {
+		stage("POLL SCM"){
+      			agent{label 'docker'}
+			steps {
+				 checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
+			}
+		}	
+					
+		stage('BUILD IMAGE') {
+       			agent{label 'docker'}
+			steps { 
+				script { 
+					dockerimage = docker.build registry + ":$dockerTag" 
+				}
+			} 
+		}
+					
+		stage('PUSH HUB') { 
+       			agent{label 'docker'}
+			steps { 
+				script {
+					docker.withRegistry( '', registryCredential ) { 
+			                	dockerImage.push() 
+                    			}
+                		}		
+			} 
+		}
+					
+		stage('DEPLOY IMAGE') {
+      			agent{label 'kubernetes'}
+			steps {
+				script { 
+					docker.withRegistry( '', registryCredential ) { 
+						dockerImage.run('-it --name "$registry-$dockerTag"') 
+					}
+				} 
+			}
+		}
+	}
+			  
+}
+
+}
---
+++ vars/kubernetesShell.groovy
@@ -0,0 +1,47 @@
+def call(String registryCred = 'a', String registryname = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a', String depname = 'a', String contname = 'a') {
+
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+    		registry = "${registryname}" 	
+		dockerTag = "${docTag}$BUILD_NUMBER"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+    		deployment = "${depname}"
+    		containerName = "${contname}"
+	}
+		
+    agent none
+
+    stages {
+        stage("POLL SCM"){
+		agent{label 'docker'}
+            		steps {
+                	checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])             
+            		}
+        } 
+        
+        stage('BUILD IMAGE') {
+		agent{label 'docker'}
+            		steps {
+                	sh 'docker build -t $registry:$dockerTag .'             
+            		}
+        }
+        
+        stage('PUSH HUB') { 
+		agent{label 'docker'}
+            		steps {
+			            sh 'docker push $registry:$dockerTag'                   	
+                	}    
+        }
+        
+        stage('DEPLOY IMAGE') {
+		agent{label 'kubernetes'}
+		          steps {
+			            sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
+		          }
+	}  
+    }
+}  
+}
